054e98b225e33c52bd355635656e8eaa912ee111
Gokul Madhavan <gokul.madhavan@post.harvard.edu>
2018-11-02 21:54:05 -0400

    Fix typo in asset path variable name

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3000,
     databaseURL: process.env.DATABASE_URL,
-    sprint_image_path: "/sprint_report/images/",
+    sprint_img_path: "/sprint_report/images/",
 };
