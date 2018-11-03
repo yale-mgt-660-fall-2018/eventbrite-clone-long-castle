dd09813461c4cb6fa3639f333714b75bd95bf170
Gokul Madhavan <gokul.madhavan@post.harvard.edu>
2018-11-02 21:34:33 -0400

    Add route for sprint report

--- src/controllers/sprint.js
+++ src/controllers/sprint.js
@@ -2,12 +2,11 @@
  * @param  {Context} ctx - A Koa Context
  * @returns {Promise} - Returns a promise that resolves to undefined
  */
-async function index(ctx) {
-    const template = 'index.njk';
-    const waysOfBeingAwesome = ['awesome1', 'awesome2', 'awesome3'];
-    return ctx.render(template, { waysOfBeingAwesome });
+async function sprint(ctx) {
+    const template = 'sprint.njk';
+    return ctx.render(template, { });
 }
 
 module.exports = {
-    index,
+    sprint,
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,9 @@
 const Router = require('koa-router');
 const indexControllers = require('../controllers/index.js');
+const sprintControllers = require('../controllers/sprint.js');
 
 const router = new Router();
 router.get('/', indexControllers.index);
+router.get('/sprint', sprintControllers.sprint);
 
 module.exports = router;
