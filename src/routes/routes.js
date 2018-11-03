56fd87ecff9ddb8341b79e981b82e3ba00668e62
Gokul Madhavan <gokul.madhavan@post.harvard.edu>
2018-11-02 22:42:16 -0400

    Remove non-static references to sprint report page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,7 @@
 const Router = require('koa-router');
 const indexControllers = require('../controllers/index.js');
-const sprintControllers = require('../controllers/sprint.js');
 
 const router = new Router();
 router.get('/', indexControllers.index);
-router.get('/sprint', sprintControllers.sprint);
 
 module.exports = router;
