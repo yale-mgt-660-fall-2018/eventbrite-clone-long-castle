0ce07530f6e0a2edc74c89ef6ade8a62687ba9b9
Gokul Madhavan <gokul.madhavan@post.harvard.edu>
2018-11-09 18:48:59 -0500

    Add some made-up events to controller for testing

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,7 +4,40 @@
  */
 async function index(ctx) {
     const template = 'index.njk';
-    const waysOfBeingAwesome = ['awesome1', 'awesome2', 'awesome3'];
+    const allEvents = [
+     {
+       id: 1,
+       title: "Awesome 1",
+       datetime: new Date(Math.random())
+     }, 
+     {
+       id: 2,
+       title: "Awesome 2",
+       datetime: new Date(Math.random())
+      },     {
+       id: 3,
+       title: "Awesome 3",
+       datetime: new Date(Math.random())
+     }, 
+     {
+       id: 4,
+       title: "Awesome 4",
+       datetime: new Date(Math.random())
+      },     {
+       id: 5,
+       title: "Awesome 5",
+       datetime: new Date(Math.random())
+     }, 
+     {
+       id: 6,
+       title: "Awesome 6",
+       datetime: new Date(Math.random())
+      },     {
+       id: 7,
+       title: "Awesome 7",
+       datetime: new Date(Math.random())
+     }, 
+    ];
     return ctx.render(template, { waysOfBeingAwesome });
 }
 
