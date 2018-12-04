2a59626ca1929acfb98d17ae3ec276491fc214f5
Owen Yang <owen.yang@yale.edu>
2018-12-04 21:51:57 +0000

    removing console log

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -5,10 +5,8 @@ const eventsModels = require('../models/events.js');
  * @returns {Promise} - Returns a promise that resolves to undefined
  */
 async function register(ctx) {
-    console.log("I am here");
     const errors = [];
     if (ctx.method === 'POST') {
-        console.log("I am here!!");
         try {
             const theEvent = await eventsModels.insert(
                 ctx.db,
@@ -17,10 +15,8 @@ async function register(ctx) {
                 ctx.request.body.picture,
                 ctx.request.body.event_location,
             );
-            console.log(theEvent);
             ctx.redirect('/events/' + theEvent.id);
         } catch (e) {
-            console.log(e);
             errors.push("there was an error saving");
         }
     }
