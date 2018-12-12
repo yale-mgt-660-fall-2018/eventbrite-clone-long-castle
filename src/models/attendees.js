47e66b39ce20e81412af7f568eaae1bbc84b2f02
Gokul Madhavan <gokul.madhavan@yale.edu>
2018-12-12 20:04:27 +0000

    Add Attendees model for talking to registrations table

---
+++ src/models/attendees.js
@@ -0,0 +1,69 @@
+/**
+ * @param {Database} db - Pg-promise database object
+ * @param  {String} event_id - Event being registered for
+ * @param  {String} email - Email id of the attendee
+ * @returns {Promise} - Promise that resolves to new row in db.
+ */
+async function insert(db, event_id, email) {
+    // Notice that our JavaScript variables are CamelCase
+    // and our SQL variables are snake_case. This is a
+    // common convention.
+    console.log("just entered attendee insert function");
+    const stmt = `
+        INSERT INTO registrations (event_id, email)
+        VALUES ($1, $2)
+        RETURNING id, event_id, email
+    `;
+    //    console.log(db.one(stmt, [event_id, date, imageURL, location]));
+    return db.one(stmt, [event_id, email]);
+}
+
+/**
+ * @param {Database} db - Pg-promise database object
+ * @returns {Promise} - Promise that resolves to and int
+ */
+async function count(db) {
+    console.log("inside attendee count");
+    const stmt = 'select COUNT(*) FROM registrations';
+    const result = await db.one(stmt);
+    console.log(result);
+    return parseInt(result.count, 10);
+}
+
+
+/**
+ * @param  {Database} db - Pg-promise database object
+ * @param  {String} searchString - String for which to search event id
+ * @returns {Promise} - Promise that resolves to one event or null
+ */
+async function getByEventID(db, searchString) {
+    console.log("just entered getByID");
+    const stmt = `
+        SELECT * FROM registrations WHERE
+        event_id = $1
+    `;
+    return db.any(stmt, [searchString]);
+}
+
+
+/**
+ * @param  {Database} db - Pg-promise database object
+ * @returns {Promise} - Promise that resolves to many events or null
+ */
+async function getAll(db) {
+    console.log("inside getAll");
+    const stmt = `
+        SELECT * FROM registrations
+    `;
+    const result = await db.any(stmt, [true]);
+    console.log(result);
+    return result;
+}
+
+
+module.exports = {
+    insert,
+    count,
+    getAll,
+    getByEventID
+};
